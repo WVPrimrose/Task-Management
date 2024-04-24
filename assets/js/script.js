4a2f1e963cebd55a3f4869df00011ffbfaa19f53
Katie Buchholz <kzbindy@hotmail.com>
2024-04-23 21:05:38 -0400

    still trying to figure out why prevent default isn't working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Retrieve tasks and nextId from localStorage
-// let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-// let nextId = JSON.parse(localStorage.getItem("nextId"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+let nextId = JSON.parse(localStorage.getItem("nextId"));
 let saveChanges = $('#save')
 
 // Todo: create a function to generate a unique task id
@@ -124,6 +124,5 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 saveChanges.on("submit",handleFormSubmit)
 $(document).ready(function () {
-  handleFormSubmit.preventDefault();
 
 });
